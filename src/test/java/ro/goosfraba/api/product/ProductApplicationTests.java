72c08984f5179e0551c498bf8955c53921f36817
gabrielradu <radu.gabi93@gmaiil.com>
2023-01-06 08:19:35 +0200

    basic spring structure

---
+++ src/test/java/ro/goosfraba/api/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.goosfraba.api.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
